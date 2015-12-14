2b9590d37888c6800d7cea97d05db331217dbc3c
yu62493 <yu62493@gmail.com>
2015-12-14 14:56:14 +0800

    modify server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var http = require("http");
 
 function start()
 {
+	function onRequest(request, response)
+	{
+		console.log("Requst received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello Tiny");
+		response.end();
+	}
+	http.createServer(onRequest).listen(process.env.PORT,process.env.IP);
+	console.log("Server has started.");
 }
 
 exports.start = start ;
